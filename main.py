e26130c7ddf19d9a297c970f1bc4e94bcf0ec6c5
projectperfect <jprfct@protonmail.com>
2022-08-07 19:31:35 -0500

    Check answer function completed.

--- main.py
+++ main.py
@@ -13,3 +13,9 @@ def format_data(account):
     country = account["country"]
     return f"{name}, a {description}, from {country}"
 
+def check_answer(guess, a_followers, b_followers):
+    """Checks followers against user's guess and returns True if they got it right. Or False if they got it wrong."""
+    if a_followers > b_followers:
+        return guess == "a"
+    else:
+        return guess == "b"
