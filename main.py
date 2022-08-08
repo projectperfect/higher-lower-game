2c0179425afd52159701fd6db65a3c6f132e77bd
projectperfect <jprfct@protonmail.com>
2022-08-07 19:29:26 -0500

    Function to format data.

--- main.py
+++ main.py
@@ -6,3 +6,10 @@ def get_random_account():
     """Get data from random account"""
     return random.choice(data)
 
+def format_data(account):
+    """Format account into printable format: name, description and country """
+    name = account["name"]
+    description = account["description"]
+    country = account["country"]
+    return f"{name}, a {description}, from {country}"
+
